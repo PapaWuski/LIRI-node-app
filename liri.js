4d2c57acb221449cf6dada766ec15896c599c9e2
Peter Wu <23109224+PapaWuski@users.noreply.github.com>
2019-09-25 09:49:09 -0700

    added movie key//added moviethis func

--- liri.js
+++ liri.js
@@ -3,38 +3,81 @@ const axios = require("axios");
 const moment = require("moment");
 const fs = require("fs");
 const Spotify = require("node-spotify-api");
+const inquirer = require("inquirer");
 const keys = require("./keys.js");
+
 const spotify = new Spotify(keys.spotify);
-const userInput = process.argv.slice(3).join(" ")
+
+const userInput = process.argv.slice(3).join(" ");
+
 const spotifySong = userInput => {
-
-    spotify
-    .search({ type: 'track', query:userInput})
+  spotify
+    .search({ type: "track", query: userInput })
     .then(function(response) {
-
-      console.log(JSON.stringify(response.tracks.items[0].artists,null,2));
+      console.log(JSON.stringify(response.tracks.items[0].artists, null, 2));
     })
     .catch(function(err) {
       console.log(err);
     });
-}
+};
 
-// response.tracks.items[0-19].{artists[0].name  name}          
-switch (process.argv[2]) {
-    case "concert-this":
-        
-        break;
-    case "spotify-this-song":
-        spotifySong(userInput)
-        break;
-    case "movie-this":
-        
-        break;
-    case "do-what-it-says":
-        
-        break;
+const movieThis = userInput => {
+    // console.log(keys.axios.omdb,userInput)
+  const url = `http://www.omdbapi.com/?t=${userInput}&y=&plot=short&apikey=${keys.axios.omdb}`;
+  axios.get(url).then(response => {
+    console.log(response);
+  }).catch(err => console.log(err));
+};
 
-    default:
+// response.tracks.items[0-19].{artists[0].name  name}
+inquirer
+  .prompt([
+    {
+      name: "response",
+      message: "What would you like LIRI to do today?",
+      type: "list",
+      choices: [
+        "Concert This",
+        "Spotify This",
+        "Movie This",
+        "Do what it says",
+        "Quit"
+      ]
+    }
+  ])
+  .then(data => {
+    switch (data.response) {
+      case "Concert This":
         break;
-}
-
+      case "Spotify This":
+        inquirer
+          .prompt([
+            {
+              name: "songName",
+              type: "input",
+              message: "What is the name of the song you are looking for?"
+            }
+          ])
+          .then(data => {
+            spotifySong(data.songName);
+          });
+        break;
+      case "Movie This":
+        inquirer
+          .prompt([
+            {
+              name: "movieName",
+              type: "input",
+              message: "What is the name of the movie you're looking for?"
+            }
+          ])
+          .then(data => {
+            movieThis(data.movieName);
+          });
+        break;
+      case "Do what it says":
+        break;
+      default:
+        break;
+    }
+  });
