9b3b9238ea86b42d49cc8b329d4cf47e0f81d124
Peter Wu <23109224+PapaWuski@users.noreply.github.com>
2019-09-26 09:23:15 -0700

    spotify this now show  console.log for first track

--- liri.js
+++ liri.js
@@ -12,7 +12,16 @@ const spotifySong = userInput => {
   spotify
     .search({ type: "track", query: userInput })
     .then(function(response) {
+      console.log(JSON.stringify(response.tracks.items[0], null, 2));
       console.log(JSON.stringify(response.tracks.items[0].artists, null, 2));
+console.log(
+`
+${response.tracks.items[0].external_urls.spotify}
+${response.tracks.items[0].artists[0].name}
+${response.tracks.items[0].name}
+${response.tracks.items[0].album.name}
+`
+)
     })
     .catch(function(err) {
       console.log(err);
@@ -20,11 +29,28 @@ const spotifySong = userInput => {
 };
 
 const movieThis = userInput => {
+  // have a default movie
   const url = `http://www.omdbapi.com/?t=${userInput}&y=&plot=short&apikey=${keys.axios.omdb}`;
   axios.get(url).then(response => {
-    console.log(response);
+console.log(`
+${response.data.Title}
+${response.data.Year}
+${response.data.imdbRating}
+${response.data.Country}
+${response.data.Language}
+${response.data.Plot}
+${response.data.Actors}`)
+    // * Title of the movie.
+    // * Year the movie came out.
+    // * IMDB Rating of the movie.
+    // * Rotten Tomatoes Rating of the movie.
+    // * Country where the movie was produced.
+    // * Language of the movie.
+    // * Plot of the movie.
+    // * Actors in the movie.
   }).catch(err => console.log(err));
 };
+
 const concertThis = userInput => {
   
   const url = `https://rest.bandsintown.com/artists/${userInput}?app_id=${keys.axios.bit}`;
@@ -33,9 +59,10 @@ const concertThis = userInput => {
   }).catch(err => console.log(err));
 };
 
-// const appendToLog = str => {
-//   fs.writeFile  
-// }
+// create logging function here
+const appendToLog = str => {
+  fs.writeFile  
+}
 
 // response.tracks.items[0-19].{artists[0].name  name}
 inquirer
